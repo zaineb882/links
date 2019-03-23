ad4e396082225360c6490c003d5779f1c0a84f03
zaineb saadoun <zsaadoun882@gmail.com>
2019-03-23 12:51:01 +0000

    commit all

---
+++ database/factories/LinkFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Link::class, function (Faker $faker) {
+    return [
+      'title' => substr($faker->sentence(2), 0, -1),
+       'url' => $faker->url,
+       'description' => $faker->paragraph,
+    ];
+});
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    $links = \App\Link::all();
+    return view('welcome', ['links' => $links]);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
